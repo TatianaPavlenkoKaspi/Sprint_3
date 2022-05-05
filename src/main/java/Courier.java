5538fb3b17a6fa9ecbb9c3335b21450aa5856d81
Tatiana Pavlenko <tatyana.pavlenko@kaspi.kz>
2022-05-05 15:58:59 +0600

    tests

---
+++ src/main/java/Courier.java
@@ -0,0 +1,10 @@
+import lombok.*;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Courier {
+    private String login;
+    private String password;
+    private String firstName;
+}
